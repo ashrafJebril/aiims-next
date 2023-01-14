f5f5da9f15f3ef516f6980f9eefed4feb96358a4
Ashraf Jebril <ajebril@aiims.com.au>
2023-01-14 14:37:54 +1100

    fix

--- components/Crousel.js
+++ components/Crousel.js
@@ -1,9 +1,35 @@
-const Crousel = () => {
+import Filters from "../components/Filters";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  {
+    url: "https://venuenow.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fyour-desk.a0ebf559.jpg&w=2048&q=75",
+  },
+  {
+    url: "https://venuenow.com/_next/image?url=%2F_next%2Fst…c%2Fmedia%2Fverandah-bar.c6c03f18.jpg&w=2048&q=75",
+  },
+  {
+    url: "https://venuenow.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fqt-sydney.7a9475f0.jpg&w=2048&q=75",
+  },
+];
+
+const Slider = () => {
   return (
-    <div className="">
-      <div className="bg-blue-600 h-96 "></div>
+    <div style={{ height: "400px" }}>
+      <SimpleImageSlider
+        width="100%"
+        height={400}
+        images={images}
+        autoPlay={true}
+        // showBullets={true}
+        // showNavs={true}
+      />
+
+      <div className="overlay text-white flex justify-center items-center">
+        <Filters />
+      </div>
     </div>
   );
 };
 
-export default Crousel;
+export default Slider;
---
+++ components/Filters.js
@@ -0,0 +1,12 @@
+const Filters = () => {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="text-4xl w-2/5 text-center font-bold mb-10">
+        Find and book your perfect corporate function venue
+      </div>
+      <div className="h-14 w-1/2 bg-white  rounded"></div>
+    </div>
+  );
+};
+
+export default Filters;
